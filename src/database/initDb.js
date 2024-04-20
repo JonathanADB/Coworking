5979f69b0c2ad9a5040ca7d552b7d0b50fb3fc6d
Daniela <dcac3012@gmail.com>
2024-04-20 16:52:59 +0200

    Endpoint register y login user,validacion de registro y login, middleware authenticacion

---
+++ src/database/initDb.js
@@ -0,0 +1,20 @@
+import "dotenv/config.js";
+
+import { createConnection } from "mysql2/promise.js";
+import { dbSchema } from "./dbSchema.js";
+import { loadBaseData } from "./loadBaseData.js";
+
+const { DB_HOST, DB_PORT, DB_USER, DB_PASS } = process.env;
+
+const db = await createConnection({
+  host: DB_HOST,
+  port: DB_PORT,
+  user: DB_USER,
+  password: DB_PASS,
+});
+
+await dbSchema(db);
+
+await loadBaseData(db);
+
+await db.end();
