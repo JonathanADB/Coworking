81910f66a3ecc23c104c8792617e394fa1968e24
Pablo Campuzano Fuente <87028354+PabloCF84@users.noreply.github.com>
2024-04-23 14:14:59 +0200

    Update routes.js

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,23 +1,28 @@
-import express from 'express';
-import confirmationRejection from './api/confirmationRejection';
-import createRoom from './api/createRoom';
-import reserve from './api/reserve';
-import registerNewUser from './api/user/registerNewUser';
-import loginUser from './api/user/loginUser';
-import validateUser from './api/user/validateUser';
-import forgotPassword from './api/user/forgotPassword'; //jhonny
-import changePassword from './api/user/changePassword'; //jhonny
-import transporter from '../utils/emailConfig';         //jhonny
+import express from "express";
+// import confirmationRejection from "./api/confirmationRejection.js";
+// import createRoom from "./api/createRoom.js";
+// import reserve from "./api/reserve.js";
+import registerNewUser from "./api/user/registerNewUser.js";
+import loginUser from "./api/user/loginUser.js";
+import validateUser from "./api/user/validateUser.js";
+import { equipmentRouter } from "./api/equipmentList.js";
+import { roomsTypesRouter } from "./api/roomsTypes.js";
+import { categoryIncidentsRouter } from "./api/categoryIncidents.js";
+import { reservationReview } from "./api/reservationReview.js";
+import { roomsList } from "./api/roomsList.js";
 
 const router = express.Router();
 
-router.use('/register', registerNewUser);
-router.use('/login', loginUser);
-router.use('/validate', validateUser);
-router.use('/forgot-password', forgotPassword); //jhony
-router.use('/change-password', changePassword); //jhonny
-router.use('/confirmationRejection', confirmationRejection);
-router.use('/createRoom', createRoom);
-router.use('/reserve', reserve);  //*jhonny
+router.use(registerNewUser);
+router.use(loginUser);
+router.use(validateUser);
+// router.use("/confirmationRejection", confirmationRejection);
+// router.use("/createRoom", createRoom);
+// router.use("/reserve", reserve);
+router.use(equipmentRouter);
+router.use(roomsTypesRouter);
+router.use(categoryIncidentsRouter);
+router.use(reservationReview);
+router.use(roomsList);
 
-export { router, transporter }; // Exporta el router y el objeto de transporte de nodemailer jhonny
+export default router;
