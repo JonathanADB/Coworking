fefa018dae2d66d4d363951b9fef5db8f77c1004
Jonathan <jhonnysoy1@gmail.com>
2024-04-25 01:27:13 +0200

    me manda actualizar

--- backend/routes/api/user/changePassword.js
+++ backend/routes/api/user/changePassword.js
@@ -1,42 +1,41 @@
-import { Router } from "express";
-import { getPool } from "../../../database/getPool.js";
-import { hash, compare } from "bcrypt";
+import { Router } from 'express';
+import { getPool } from '../../../database/getPool.js';
+import { hash, compare } from 'bcrypt';
 
 const pool = getPool();
 
 export const changePassword = Router();
 
-changePassword.post("/change-password", async (req, res, next) => {
+changePassword.patch('/change-password', async (req, res, next) => {
   try {
     const { userId, currentPassword, newPassword } = req.body;
 
-    const [user] = await pool.query("SELECT * FROM users WHERE id = ?", [
+    const [user] = await pool.query('SELECT * FROM users WHERE id = ?', [
       userId,
     ]);
 
     if (!user) {
       return res
         .status(404)
-        .json({ success: false, message: "Usuario no encontrado" });
+        .json({ success: false, message: 'Usuario no encontrado' });
     }
 
     const passwordMatch = await compare(currentPassword, user.password);
     if (!passwordMatch) {
       return res.status(400).json({
         success: false,
-        message: "La contraseña actual es incorrecta",
+        message: 'La contraseña actual es incorrecta',
       });
     }
 
     const hashedNewPassword = await hash(newPassword, 12);
 
-
-    await pool.execute("UPDATE users SET password = ? WHERE id = ?", [
+    await pool.execute('UPDATE users SET password = ? WHERE id = ?', [
       hashedNewPassword,
       userId,
     ]);
 
-    res.json({ success: true, message: "Contraseña actualizada exitosamente" });
+    res.json({ success: true, message: 'Contraseña actualizada exitosamente' });
   } catch (error) {
     next(error);
   }
