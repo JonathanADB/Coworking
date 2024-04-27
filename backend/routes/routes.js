b76bd2e78ec8dcbb80436fa93bb8002c377d3272
Danielaal3012 <dcac3012@gmail.com>
2024-04-27 18:30:03 +0200

    rutas arregladas y verificacion de todos los endpoint listos+ coleccion de postman

--- backend/routes/routes.js
+++ backend/routes/routes.js
@@ -1,35 +1,27 @@
 import express from "express";
-import registerNewUser from "./api/user/registerNewUser.js";
-import loginUser from "./api/user/loginUser.js";
-import validateUser from "./api/user/validateUser.js";
-//import confirmationRejection from "./api/confirmationRejection.js";
-//import createSpace from "./api/createSpace.js";
-import {forgotPassword} from "./api/user/forgotPassword.js";
-import {changePassword} from "./api/user/changePassword.js";
-//import transporter from "../utils/emailConfig.js";
-import { equipmentRouter } from "./api/equipmentList.js";
-import { roomsTypesRouter } from "./api/roomsTypes.js";
-import { categoryIncidentsRouter } from "./api/categoryIncidents.js";
-import { reviewController } from "./api/reviewController.js";
-import { roomsController } from "./api/roomsController.js";
-//import reservationRouter from "./api/reservationSpace.js";
+
+import { userRouter } from "./api/user/user.js";
+import { equipmentAdminRouter } from "./api/admin/equipmentAdmin.js";
+import { roomsRouter, roomsTypesRouter, reservationRouter } from "./api/rooms/rooms.js";
+import { categoryIncidentsRouter,listIncidentsRouter } from "./api/incidents/incidents.js";
+import { reviewController } from "./api/review/review.js";
+import { roomsController } from "./api/rooms/roomsController.js";
+
+
 
 const router = express.Router();
 
-router.use(registerNewUser);
-router.use(loginUser);
-router.use(validateUser);
-router.use(forgotPassword);
-router.use(changePassword);
-//router.use(confirmationRejection);
-//router.use(createSpace);
-//router.use(reservationRouter);
-router.use(equipmentRouter);
+router.use(userRouter);
+router.use(equipmentAdminRouter);
+router.use(roomsRouter);
 router.use(roomsTypesRouter);
+router.use(reservationRouter);
 router.use(categoryIncidentsRouter);
+router.use(listIncidentsRouter);
 router.use(reviewController);
 router.use(roomsController);
 
+
+
 export default router;
 
-//export { router, transporter };
--- backend/server.js
+++ backend/server.js
@@ -7,12 +7,14 @@ import notFoundHandler from "./routes/middleware/notFoundHandler.js";
 import serveStatic from "./routes/middleware/serveStatic.js";
 //import authenticate from "./routes/middleware/authenticateTokenUser.js";
 import router from "./routes/routes.js";
+//import isAdmin from "./routes/middleware/isAdmin.js";
 
 const app = express();
 
 //app.use(handleCors);
 app.use(bodyParser);
 //app.use(authenticate);
+//app.use(isAdmin);
 //app.use(handleFileUpload);
 app.use(serveStatic);
 
