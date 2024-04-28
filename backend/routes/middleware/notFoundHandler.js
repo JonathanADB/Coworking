afa9cebbef37da0eab3ec6a3cf97e895a656530c
Pablo Campuzano Fuente <87028354+PabloCF84@users.noreply.github.com>
2024-04-28 21:05:14 +0200

    Delete backend directory

--- backend/routes/middleware/notFoundHandler.js
+++
@@ -1,7 +0,0 @@
-const notFoundHandler = (req, res, next) => {
-    const error = new Error('Recurso no encontrado');
-    error.status = 404;
-    next(error);
-  };
-  
-export default notFoundHandler;
