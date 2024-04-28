18792613a02db7cce4f7d0b05edf1e33e8671bab
Pablo Campuzano Fuente <87028354+PabloCF84@users.noreply.github.com>
2024-04-28 21:07:19 +0200

    Add files via upload

---
+++ backend/validations/validateReviewId.js
@@ -0,0 +1,14 @@
+import { getPool } from "../database/getPool.js";
+
+const pool = getPool();
+
+export async function validateReviewId(reviewId) {
+  // Comprobamos y validamos si existe una review
+  const [review] = await pool.execute("SELECT * FROM reviews WHERE id = ?", [
+    reviewId,
+  ]);
+  if (!review) {
+    throw new Error("Review no encontrada");
+  }
+  return review;
+}
