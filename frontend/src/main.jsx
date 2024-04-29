dde1238a155981edab450be9301f8fbf34acce21
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-04-29 18:07:40 +0100

    Frontend AppRouter

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './components/App.jsx'
+import AppRoutes from './routes/index.jsx'
 import './styles/global.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <>
-    <App />
+    <AppRoutes />
   </>,
 )
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import App from '../components/App';
+
+const Home = () => {
+  return (
+    <div>
+        <App />
+    </div>
+  );
+};
+
+export default Home;
