0230f7e4765956f4f81ab3b1de441efb2953aa88
Danielaal3012 <dcac3012@gmail.com>
2024-05-08 15:54:27 +0200

    detalle de archivos

--- backend/routes/middleware/bodyParser.js
+++ backend/routes/middleware/bodyParser.js
@@ -9,16 +9,18 @@ const bodyParser = (req, res, next) => {
       req.on('data', chunk => {
         data += chunk;
       });
-  
       req.on('end', () => {
         try {
+          console.log('Data received:', data); // Registra los datos recibidos para verificar el contenido.
           req.body = JSON.parse(data);
           next();
-        } catch (error) {
-          throw createError(400, "Erro al analizar el cuerpo de la solicitud");
+        } catch (error){
+          throw createError(400, "Error al analizar el cuerpo de la solicitud");
         }
       });
-    } 
+    } else {
+      next();
+    }
   } catch (error) {
     next(error)
   }
