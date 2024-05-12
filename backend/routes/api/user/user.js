40d3e3bf9cf1abede233a4b02282c1da46cb2050
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-05-12 23:04:18 +0100

    Update user.js

--- backend/routes/api/user/user.js
+++ backend/routes/api/user/user.js
@@ -34,11 +34,9 @@ userRouter.post("/register", async (req, res, next) => {
     if (error) {
       throw createError(400, "Datos de entrada no válidos");
     }
-    console.log(req.body);
     const { firstName, lastName, username, email, password } = await validateRegisterRequest(
       req.body
     );
-    console.log(firstName, lastName, username, email, password);
     const userId = crypto.randomUUID();
     const hashedPassword = await hash(password, 12);
     // let firstname = firstName || null;
@@ -159,9 +157,7 @@ userRouter.get("/user/profile", authenticate, async (req, res, next) => {
 userRouter.put("/user/update/profile/", authenticate, async (req, res, next) => {
   try {
     const user = await getUser(req.headers.authorization);
-    console.log(user);
     const { firstName, lastName, username, email } = req.body;
-    console.log(req.body)
     const avatarFile = req.file?.avatar || null; // falta el uploadFile de Avatar
     const { error } = profileSchema.validate({
       firstName,
