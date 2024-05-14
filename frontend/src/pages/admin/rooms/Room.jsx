495a99bb6a21d153313b8cebc8475b2597ce339b
Jorge Lehmann <32737057+Zhanth@users.noreply.github.com>
2024-05-14 13:46:56 +0100

    Update Room.jsx

--- frontend/src/pages/admin/rooms/Room.jsx
+++ frontend/src/pages/admin/rooms/Room.jsx
@@ -31,6 +31,35 @@ const Room = () => {
 
     console.log(room)
 
+    return (
+        <div className="flex flex-col justify-center w-full p-4">
+            {room && room && (
+                <div>
+                    <h2 className="mb-8 text-xl font-bold">
+                        Sala {room.id}
+                    </h2>
+                    <ul className="flex flex-col gap-y-4">
+                        <li>
+                            <span className="font-bold">ID:</span> {room.id}
+                        </li>
+                        <li>
+                            <span className="font-bold">Nombre:</span> {room.name}
+                        </li>
+                        <li>
+                            <span className="font-bold">Descripción:</span> {room.description}
+                        </li>
+                        <li>
+                            <span className="font-bold">Capacidad:</span> {room.capacity}
+                        </li>
+                        <li>
+                            <span className="font-bold">Tipo:</span> {room.typeOf}
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    )
+
 };
 
 export default Room;
